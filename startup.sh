37fe488ad2c366a60a38c8dce67686f0f6394ee0
Majd Hamoudah <majhamou@amazon.com>
2024-05-12 02:52:18 -0600

    added #!/bin/sh to start.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 # startup.sh is used by infra/resources.bicep to automate database migrations
-
+#!/bin/sh
 flask db upgrade
 gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
